832f44338ccb5f436ba8fd86e98eccb5ff38eb3c
Christopher Hugh Kelly <christopher.kelly1997@gmail.com>
2023-10-29 02:42:26 -0600

    this tests regex for specific strings

--- lib/regex.py
+++ lib/regex.py
@@ -1,5 +1,5 @@
 import re
 
-my_pattern = r""
-my_regex = re.compile(my_pattern)
 
+my_pattern = r"It\'s\ssuch\sa\slovely\sday\stoday\.|Some\sweather\swe\'re\shaving\stoday\,\shuh\?|Maybe\stoday\'s\sjust\snot\smy\sday\.";
+my_regex = re.compile(my_pattern)
